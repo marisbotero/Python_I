36ab88292701317902ffd03f80be1af559fe7812
marisbotero <maris2143@gmail.com>
2017-08-18 10:54:48 -0500

    practical exercises

---
+++ aleatorio.py
@@ -0,0 +1,27 @@
+# -*- coding: utf-8 -*-
+import random 
+
+
+def run():
+    number_found = False
+    random_number = random.randint(0,20)
+
+
+    while not number_found:
+        number = int (input('Intenta un número:'))
+
+        if number == random_number:
+        	print('Felicidades encontraste el número =)')
+        	number_found = True
+        elif number > random_number:
+        	print ('El numero es más pequeño')
+        else:
+        	print ('El número es más grande')
+
+
+
+
+
+
+if __name__ == '__main__': 
+	run()
