2fee8b54d2e0ead41687068cbf45a8521e46674a
marisbotero <maris2143@gmail.com>
2019-04-28 11:50:02 -0500

    🧡

---
+++ Hackerrank/loop.py
@@ -0,0 +1,15 @@
+import math
+import os
+import random
+import re
+import sys
+
+
+    
+
+def sol(n):
+    for i in range(1, 11):
+        print(str(n) + " x " + str(i) + " = " + str(n * i))
+
+if __name__ == '__main__':
+    n = int(input())
