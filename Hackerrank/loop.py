e698d01932637434de83bbe1b0e51c09475b2f35
marisbotero <maris2143@gmail.com>
2019-04-28 12:25:51 -0500

    💚

--- Hackerrank/loop.py
+++ Hackerrank/loop.py
@@ -5,11 +5,11 @@ import re
 import sys
 
 
-    
+def loop(n):
+     for i in range(1, 11):
+         print(str(n + " x " + str(i) + " = " + str(n* i))
 
-def sol(n):
-    for i in range(1, 11):
-        print(str(n) + " x " + str(i) + " = " + str(n * i))
 
 if __name__ == '__main__':
     n = int(input())
+    loop(n)
