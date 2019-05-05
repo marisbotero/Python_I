65553c145b95e4bd2342c62ce97b190181459669
marisbotero <maris2143@gmail.com>
2019-05-05 12:49:53 -0500

    🖤

--- Hackerrank/array.py
+++ Hackerrank/array.py
@@ -1,3 +1,5 @@
+#!/bin/python3
+
 import math
 import os
 import random
@@ -5,8 +7,18 @@ import re
 import sys
 
 
+def inverse (arr):
+    arr.reverse()
+    
+    for num in arr:
+        a =  str(num)+ " "
+      
+        print(a, sep=' ', end='', flush=True)
+
 
 if __name__ == '__main__':
     n = int(input())
 
     arr = list(map(int, input().rstrip().split()))
+
+    inverse(arr)
