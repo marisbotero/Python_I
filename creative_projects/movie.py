7b6042cc200bab9aa027642454380a1e93e5135b
marisbotero <maris2143@gmail.com>
2019-07-27 15:15:22 -0500

    💚

--- creative_projects/movie.py
+++ creative_projects/movie.py
@@ -1,10 +1,12 @@
 #title
 #storyline
 
-def show_trailes():
+def show_trailes(title, youtube_url):
+    #Open browswer and play trailes
     pass
 
-def show_info():
+def show_info(title, storyline, release_data, rating,youtobe_url, director, box_office):
+    #Print movie information
     pass
 
 
