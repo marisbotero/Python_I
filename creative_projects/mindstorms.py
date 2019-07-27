ef590e4805cac0b4a93b2c21f0708ebbe21edc3e
marisbotero <maris2143@gmail.com>
2019-07-27 17:27:06 -0500

    💚

--- creative_projects/mindstorms.py
+++ creative_projects/mindstorms.py
@@ -13,6 +13,12 @@ def draw_square():
     brad.right(90)
     brad.forward(100)
     brad.right(90)
+    
+
+    angie = turtle.Turtle()
+    angie.shape()
+    angie.color('violet')
+    angie.circle(100)
     window.exitonclick()
 
 draw_square()
