3838336dda587e15c11c7ca23f30a276b44fb5a8
marisbotero <maris2143@gmail.com>
2019-07-27 17:00:04 -0500

    💚

--- creative_projects/mindstorms.py
+++ creative_projects/mindstorms.py
@@ -1,2 +1,18 @@
+import turtle
+
 def draw_square():
-    pass
+    window = turtle.Screen()
+    window.bgcolor('pink')
+   
+    brad.shape('Turtle')
+    brad.forward(100)
+    brad.right(90)
+    brad.forward(100)
+    brad.right(90)
+    brad.forward(100)
+    brad.right(90)
+    brad.forward(100)
+    brad.right(90)
+    window.exitonclick()
+
+draw_square()
